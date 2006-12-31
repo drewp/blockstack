538885ff6988fff566b7fbe07f4ef733aaac49be
Drew Perttula <drewp@bigasterisk.com>
2006-12-31 02:33:24 -0800

    split into more files
    darcs-hash:20061231103324-836b1-04a2142d510f223f322900e9d831a0a07b300a9e.gz

---
+++ multiimage.py
@@ -0,0 +1,35 @@
+from __future__ import division
+import pygame
+import Image, ImageFilter
+import Numeric as num
+
+class MultiImage(object):
+    def __init__(self, string, w, h):
+        self.w, self.h = w, h
+        self.string = string[:self.w * self.h * 3]
+
+    def size(self):
+        return self.w, self.h
+        
+    def asArray(self):
+        a = num.fromstring(self.string, num.UnsignedInt8)
+        a.shape = self.h, self.w, 3
+        return a
+
+    def asSurface(self):
+        return pygame.image.fromstring(self.string, (self.w, self.h), "RGB")
+
+    def asImage(self):
+        return Image.fromstring("RGB", (self.w, self.h), self.string)
+
+
+def writeArray():
+    raise NotImplementedError
+    h,w = L.shape
+    b = num.repeat(L, 3, 1)
+    b.shape = h,w,3   
+    Image.fromstring("RGB",
+                     (b.shape[1], b.shape[0]),
+                     num.clip(b, 0, 255).astype('b').tostring()
+                     ).save("diff.ppm")
+    
---
+++ vision.py
@@ -0,0 +1,48 @@
+from __future__ import division
+import Image
+import Numeric as num
+from multiimage import MultiImage
+
+colors = dict(red=(203, 131, 146),
+              blue=(135, 148, 199),
+              green=(37, 102, 118))
+
+def findBlocks(multiImage, feedbackSurf=None):
+    """colorName : (x,y)
+    in relative (0 to 1) coords. Draws on the optional pygame surface"""
+    
+    thumb = multiImage.asImage()
+    thumb.thumbnail((43//2, 36//2), Image.ANTIALIAS)
+
+    a = MultiImage(thumb.tostring(), thumb.size[0], thumb.size[1]).asArray()
+
+    ret = {}
+    for colorName, detectColor in colors.items():
+        b = num.absolute(a.astype('f') - detectColor)
+
+        L = num.sum(b, -1)
+
+        goodMatch = L < 100
+
+        hits = 0
+        ctr = num.zeros(2)
+
+        for y in range(a.shape[0]):
+            for x in range(a.shape[1]):
+                if goodMatch[y][x]:
+                    if feedbackSurf:
+                        x1, y1 = (num.array([x, y]) /
+                                  [a.shape[1], a.shape[0]] * multiImage.size())
+                        feedbackSurf.fill((0,0,0), (x1-1, y1-1, 10, 10))
+                        feedbackSurf.fill(detectColor, (x1, y1, 8, 8))
+                    hits += 1
+                    ctr += [x, y]
+        if hits > .10 * (a.shape[0] *  a.shape[1]):
+            ctr = ctr / hits
+            ret[colorName] = ctr / [a.shape[1], a.shape[0]]
+
+            if feedbackSurf:
+                x, y = ctr / [a.shape[1], a.shape[0]] * multiImage.size()
+                feedbackSurf.fill((255,255,255), (x, y, 10, 10))
+
+    return ret
