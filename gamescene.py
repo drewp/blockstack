1a01309a544151d564f5417e262a9d73f0db02d3
Drew Perttula <drewp@bigasterisk.com>
2012-03-11 23:26:57 -0700

    clean up old code. opengl is drawing wrong
    Ignore-this: e760031bf984728df2455e55091b51ad
    
    darcs-hash:20120312062657-836b1-05e3ebbf868c34963db9bf489b5bb56fedb7c847.gz

--- gamescene.py
+++ gamescene.py
@@ -7,8 +7,6 @@ import random, sys
 import gtk
 import time
 
-import vision
-
 def cube(color=(1,1,1), center=(0,0,0), side=1, wire=False):
     glColor3f(*color)
     glPushMatrix()
@@ -25,7 +23,11 @@ def groundPlane():
     cube(color=(.2, .2, 1))
     glPopMatrix()
 
-
+colors = {
+    'red' : [255,0,0],
+    'green' : [0,255,0],
+    'blue' : [0,0,255],
+    }
 
 class GameScene(GLScene):
     def init(self):
@@ -34,37 +36,25 @@ class GameScene(GLScene):
                          gtk.gdkgl.MODE_DEPTH |
                          gtk.gdkgl.MODE_DOUBLE)
 
-        self.positions2 = {}
-        
+        self.pose = {}
         GLUT.glutInit(sys.argv)
 
-        glClearColor (0.0, 0.0, 0.0, 0.0)
-        glShadeModel (GL_SMOOTH)
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+#        glShadeModel(GL_SMOOTH)
         glEnable(GL_COLOR_MATERIAL)
 
         glLightfv(GL_LIGHT0, GL_AMBIENT, [0.0, 0.0, 0.0, 1.0])
         glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 1.0, 1.0, 1.0])
-        glLightfv(GL_LIGHT0, GL_POSITION, [0.0, 3.0, 3.0, 0.0])
+        glLightfv(GL_LIGHT0, GL_POSITION, [0.0, 3.0, 6.0, 0.0])
         glLightModelfv(GL_LIGHT_MODEL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
-        glLightModelfv(GL_LIGHT_MODEL_LOCAL_VIEWER, [0])
 
-        #glFrontFace(GL_CW)
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
-        #glEnable(GL_AUTO_NORMAL)
-        #glEnable(GL_NORMALIZE)
-        glEnable(GL_DEPTH_TEST) 
+#        glEnable(GL_DEPTH_TEST)
+        glCullFace(GL_BACK)
+        glEnable(GL_CULL_FACE)
 
-        self.cardList = glGenLists(1)
-        glNewList(self.cardList, GL_COMPILE)
-        glColor3f(1,1,1)
-        glBegin(GL_QUADS)
-        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, 1.0,  1.0)
-        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, 1.0,  1.0)
-        glEnd()
-        glEndList()
+        self.cardList = self.makeCard()
 
     def reshape(self, width, height):
         glViewport (0, 0, width, height)
@@ -79,63 +69,35 @@ class GameScene(GLScene):
         glClearColor(0.0, 0.0, 0.0, 0.0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity ()
-        GLU.gluLookAt (0.0, 0.7, 8.0,
+        GLU.gluLookAt (0.0, 2.0, 5.0,
                        0.0, 0.5, 0.0,
                        0.0, 1.0, 0.0)
 
         glDisable(GL_TEXTURE_2D)
 
-        #global positions2, correctFrameStart
-
         groundPlane()
 
-        matching = False
-        glPushMatrix()
-        if 0:
-            glTranslatef(1, -.9, 3.2)
-            # flip to compensate for the camera facing the user
-            glScalef(-1, 1, 1)
-
-            glDisable(GL_LIGHTING)
-            glEnable(GL_TEXTURE_2D)
-
-            # without threads, i call grab() here
-            #grab.grab()
-
-            if 0:#grab.lastFrame:
-                self.imageCard(grab.lastFrame)
-                if grab.lastPosition:
-                    for colorName, pos in grab.lastPosition.items():
-                        cube(color=(x / 255 for x in vision.colors[colorName]),
-                             center=(pos[0] * 2 - 1, pos[1] * 2 - 1, 1),
-                             side=.1, wire=True)
-
-                    matching = vision.comparePose(grab.lastPosition, positions2)
-
-            glEnable(GL_LIGHTING)
-            glDisable(GL_TEXTURE_2D)
-        glPopMatrix()
-
-        for name, pos in self.positions2.items():
-            color = num.array(vision.colors[name]) / 255
-            if matching:
-                color = [.7 + .3 * x for x in color]
+        for name, pos in self.pose.items():
+            color = num.array(colors[name]) / 255
             cube(color=color, center=pos)
-
-        if matching:
-            if correctFrameStart is None:
-                correctFrameStart = time.time()
-            if time.time() > correctFrameStart + .5:
-                self.positions2 = newPositions()
-                correctFrameStart = None
-                beep.play()
-        else:
-            correctFrameStart = None
-
+            cube(color=color, center=pos, wire=True)
 
         glFlush()
 
         print "draw", time.time() - t1
+
+    def makeCard(self):
+        n = glGenLists(1)
+        glNewList(n, GL_COMPILE)
+        glColor3f(1,1,1)
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, 1.0,  1.0)
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, 1.0,  1.0)
+        glEnd()
+        glEndList()
+        return n
 
     def imageCard(self, multiImage):
         """card facing +Z from -1<x<1 -1<y<1"""
